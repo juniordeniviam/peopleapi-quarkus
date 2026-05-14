f331c2514a198ec2f0ec914d8d19f81979be1884
juniordeniviam <suportejr@outlook.com>
2026-05-14 17:34:36 -0300

    Create GET and POST Users

--- src/main/java/dev/java/controller/UserController.java
+++ src/main/java/dev/java/controller/UserController.java
@@ -1,14 +1,32 @@
 package dev.java.controller;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import domain.Users;
+import jakarta.transaction.Transactional;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.Response;
+import service.UserService;
 
 @Path("/users")
 public class UserController {
 
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
     @GET
-    public String hello(){
-        return "Hello from UserController!";
+    public Response findAllUsers(@QueryParam("page") @DefaultValue("0") Integer page,
+                                 @QueryParam("pageSize") @DefaultValue("10") Integer pageSize){
+
+        var users = Users.findAll().page(page, pageSize).list();
+        return Response.ok(users).build();
+    }
+
+    @POST
+    @Transactional
+    public Response createUsers(Users users){
+        return Response.ok(userService.createUsers(users)).build();
     }
 
 }
