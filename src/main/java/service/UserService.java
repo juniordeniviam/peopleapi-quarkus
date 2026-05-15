665a51b74f5569af1d8b7e78c6b1535525bb2221
juniordeniviam <suportejr@outlook.com>
2026-05-15 18:26:46 -0300

    create method findUserById in UserService

--- src/main/java/service/UserService.java
+++ src/main/java/service/UserService.java
@@ -1,9 +1,11 @@
 package service;
 
 import domain.Users;
+import exceptions.UserNotFoundException;
 import jakarta.enterprise.context.ApplicationScoped;
 
 import java.util.List;
+import java.util.UUID;
 
 @ApplicationScoped
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
         return Users.findAll()
                 .page(page, pageSize)
                 .list();
+    }
+
+    public Users findUserById(UUID userId){
+        return (Users) Users.findByIdOptional(userId)
+                .orElseThrow(UserNotFoundException::new);
     }
 }
